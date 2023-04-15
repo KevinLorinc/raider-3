34e6c4f5944d334a970ed834e0553f065a764a69
KevinLorinc <kevin.d.lorinc@gmail.com>
2023-04-15 15:37:01 -0500

    bum

--- raider-3-lib/src/main/java/raider/Main.java
+++ raider-3-lib/src/main/java/raider/Main.java
@@ -23,6 +23,14 @@ public class Main {
 		System.out.println("fuck you");
 		System.out.println("fuck you");
 		System.out.println("fuck you");
+		System.out.println("fuck you");
+		System.out.println("fuck you");
+		System.out.println("fuck you");
+		System.out.println("fuck you");
+		System.out.println("fuck you");
+		System.out.println("fuck you");
+		System.out.println("fuck you");
+		System.out.println("fuck you");
 		Game.info().setName("Raider");
 		Game.info().setVersion("v1.0.1");
 			
