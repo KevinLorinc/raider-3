51a90044157a914819102f9b69e7f3cff8417d0f
KevinLorinc <kevin.d.lorinc@gmail.com>
2023-04-28 10:40:16 -0500

    movement controller updated

--- raider-3-lib/src/main/java/entities/MinionController.java
+++ raider-3-lib/src/main/java/entities/MinionController.java
@@ -1,10 +1,12 @@
 package entities;
 
+import de.gurkenlabs.litiengine.Game;
 import de.gurkenlabs.litiengine.entities.behavior.AStarGrid;
 import de.gurkenlabs.litiengine.entities.behavior.AStarPathFinder;
 import de.gurkenlabs.litiengine.entities.behavior.EntityNavigator;
 import de.gurkenlabs.litiengine.physics.MovementController;
 import raider.RaidersLogic;
+import raider.RaidersLogic.GameState;
 
 /**
  * a class that handles movement for the minion enemy
@@ -14,6 +16,8 @@ public class MinionController extends MovementController<Minion>{
 	private static final int NAVIGATE_DELAY = 500;
 	private EntityNavigator navi;
 	private long lastNavigate;
+	
+	private boolean removed;
 	
 	/**
 	 * creates a new movement controller for a minion
@@ -31,6 +35,41 @@ public class MinionController extends MovementController<Minion>{
 	 */
 	@Override
 	public void update(){
-		
+		super.update();
+
+	    if (RaidersLogic.getState() != GameState.INGAME) {
+	      return;
+	    }
+
+	    if (this.getEntity().getTarget() == null || this.removed) {
+	      return;
+	    }
+	    
+	    //might have to add code here if we want the navigator to stop if the enemy is hit
+	    
+	    if(this.getEntity().getHitPoints().getRelativeCurrentValue() >= 0) {
+	    	removed = true;
+	        Game.loop().perform(1000, () -> {
+	          Game.world().environment().remove(this.getEntity());
+	        });
+	        return;
+	    }
+	    
+	    if (this.getEntity().getTarget().isDead()) {
+	        return;
+	    }
+	    
+	    if (Game.time().since(this.lastNavigate) < NAVIGATE_DELAY) {
+	        return;
+	    }
+	    
+	    double dist = this.getEntity().getTarget().getCenter().distance(this.getEntity().getCenter());
+	    if(dist > 10 && !this.navi.isNavigating()) { //will have to change this to account for hit box and what we want the range of minion to be
+	    	this.navi.navigate(this.getEntity().getTarget().getCenter());
+	    } else  if (this.navi.isNavigating()){
+	    	this.navi.stop();
+	    }/* else if (this.getEntity().getStabAbility().canCast()) {
+	    	this.getEntity().getStabAbility().cast();
+	    }*///this will be used to cast the ability when the enemy is within a certain range of the player
 	}
 }
